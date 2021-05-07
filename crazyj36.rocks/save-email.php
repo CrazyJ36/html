ebee482151a47a18fcbb24cac2b0d765500f9d26
CrazyJ36 <crazy_j36@hotmail.com>
2021-05-06 22:28:26 -0400

    site: improve email signups 2

--- crazyj36.rocks/save-email.php
+++ crazyj36.rocks/save-email.php
@@ -1,14 +1,16 @@
 <?php
-    if (isset($_POST['email']) && $_POST['email'] > 0 && preg_match("/[@]/", $_POST['email'])) {
-        file_put_contents('emails.txt', $_POST['email']. "\n", FILE_APPEND);
-        // all echos get routed back to the results div as html data in post, due to js script by form.
+    $email_data = $_POST['email_value'];
+    if (isset($email_data) && preg_match("/[@]/", $email_data)) {
+        file_put_contents('emails.txt', $email_data . "\n", FILE_APPEND);
+        // all echos get routed back to the email-label id as html data in this post execution, says to in js script for form.
         // except the alert scripts, as those aren't text data.
-        $msg = "Thank you! ".$_POST['email']." has been added. If you\'d ever like to unsubscribe from the news, simply request this by replying to any email from CrazyJ36.";
-        // maybe the only way to access php variable inside javascript that's already in php.
-        echo "<script>var php_var = '$msg'; alert(php_var);</script>";
-        echo "You signed up with: <br>". $_POST['email']."<br/>";
+        $msg = "Thank you! ". $email_data ." has been added. If you\'d ever like to unsubscribe from the news, simply request this by replying to any email from CrazyJ36.";
+        // maybe the only way to access php variable inside javascript that's already in written php.
+        echo "<script>var php_var = '$msg'; alert(php_var);</script>"; // echoing A script only, so this gets returned to js as data that runs a script.
+        echo "You've signed up for emails' to: <br>". $email_data; // echoing text that can be put into email.
     } else {
         echo "<script>alert('Please enter an email address in the email field.')</script>";
-    } 
-    return;
+        echo "Signup to hear about new music first:"; // reset email-label to default
+    } return;
+    
 ?>
